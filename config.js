27853e8b55509e31a0b11319d4f555fa936db794
Alberto Garcia de Paredes <agarciadk@gmail.com>
2017-01-12 22:14:54 +0100

    Mas cambios a config

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
   port: process.env.OPENSHIFT_NODEJS_PORT || 3000,
-  server_ip_address: process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1',
+  server_ip_address: process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
   db: process.env.MONGO_URL || 'mongodb://localhost:27017/shop',
   SECRET_TOKEN: '1q2w3e4r5t6y;H0sT14'
 }
