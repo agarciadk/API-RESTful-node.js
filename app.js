7ca04ada3c6ee2c1cdf3e27cbd39b6a6fcf4254c
Alberto Garcia de Paredes <agarciadk@gmail.com>
2017-01-13 15:38:34 +0100

    Limpiando codigo

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.set('port', (process.env.PORT || 5000));
+
 
 // Routes
 app.use('/', index)
